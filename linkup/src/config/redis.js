8e4847904f9ab45526edb655fdfed0518ce1c44c
AjalaJoseph <ajalaoluwafikayomi27@gmail.com>
2026-06-29 09:57:16 +0100

    Update redis.js

--- linkup/src/config/redis.js
+++ linkup/src/config/redis.js
@@ -2,8 +2,8 @@ import Redis from "ioredis";
 import dotenv from 'dotenv'
 dotenv.config()
 export const redis = new Redis({ 
-    host:process.env.redis_host,
-     port:process.env.redis_port,
+      host:process.env.redis_host,
+      port:process.env.redis_port,
       maxRetriesPerRequest: null,
       connectTimeout: 5000,
       password:process.env.redis_password,
